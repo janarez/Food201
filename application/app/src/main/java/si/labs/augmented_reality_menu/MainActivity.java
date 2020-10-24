d20235eee734aaa13d2dde43c1ff7b17245f8c9d
klemen kobau <klemen.kobau@gmail.com>
2020-10-24 10:21:48 +0200

    initial commit for the application

---
+++ application/app/src/main/java/si/labs/augmented_reality_menu/MainActivity.java
@@ -0,0 +1,14 @@
+package si.labs.augmented_reality_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
