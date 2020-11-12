71b104e817e827c49c9427863fae37a1528e1fa6
klemen kobau <klemen.kobau@gmail.com>
2020-11-12 16:58:02 +0100

    embraced sceneform

--- application/app/src/main/java/si/labs/augmented_reality_menu/ARActivity.java
+++ application/app/src/main/java/si/labs/augmented_reality_menu/ARActivity.java
@@ -1,87 +1,53 @@
 package si.labs.augmented_reality_menu;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.opengl.GLSurfaceView;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
-import si.labs.augmented_reality_menu.helpers.ar.ARSessionHelper;
-import si.labs.augmented_reality_menu.helpers.FullScreenHelper;
-import si.labs.augmented_reality_menu.helpers.CameraPermissionHelper;
-import si.labs.augmented_reality_menu.helpers.opengl.DisplayRotationHelper;
-import si.labs.augmented_reality_menu.helpers.opengl.GeneralRenderer;
+import com.google.ar.sceneform.ux.ArFragment;
 
 public class ARActivity extends AppCompatActivity {
+    private static final double MIN_OPENGL_VERSION = 3.0;
+    private static final String TAG = ARActivity.class.getSimpleName();
 
-    // Rendering. The Renderers are created here, and initialized when the GL surface is created.
-    private GLSurfaceView surfaceView;
-
-    private ARSessionHelper arSessionHelper;
-    private DisplayRotationHelper displayRotationHelper;
+    private ArFragment arFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_a_r);
 
-        surfaceView = findViewById(R.id.surfaceview);
-        displayRotationHelper = new DisplayRotationHelper(this);
-        arSessionHelper = new ARSessionHelper(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        requestCameraPermission();
-        arSessionHelper.onActivityResume();
-        new GeneralRenderer(surfaceView, arSessionHelper.getSession(), getAssets(), displayRotationHelper);
-        surfaceView.onResume();
-        displayRotationHelper.onResume();
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (arSessionHelper.getSession() != null) {
-            displayRotationHelper.onPause();
-            surfaceView.onPause();
-            arSessionHelper.onActivityPause();
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return;
         }
+        setContentView(R.layout.activity_a_r_sceneform);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
     }
 
-    @Override
-    protected void onDestroy() {
-        arSessionHelper.onActivityDestroy();
-        super.onDestroy();
-    }
-
-    private void requestCameraPermission() {
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            CameraPermissionHelper.requestCameraPermission(this);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] results) {
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            Toast.makeText(this, "Camera permission is required to run this application", Toast.LENGTH_LONG)
+    /**
+     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+     * on this device.
+     *
+     * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+     *
+     * <p>Finishes the activity if Sceneform can not run
+     */
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
                     .show();
-            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-                // Permission denied with checking "Do not ask again".
-                CameraPermissionHelper.launchPermissionSettings(this);
-            }
-            finish();
+            activity.finish();
+            return false;
         }
+        return true;
     }
-
 }
--- application/app/src/main/java/si/labs/augmented_reality_menu/helpers/opengl/Renderer.java
+++
@@ -1,7 +0,0 @@
-package si.labs.augmented_reality_menu.helpers.opengl;
-
-public interface Renderer {
-    void onSurfaceCreated();
-    void onSurfaceChanged(int width, int height);
-    void onDrawFrame();
-}
