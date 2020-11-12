869c0293c97c3243254384867eb91f35b0df5f91
klemen kobau <klemen.kobau@gmail.com>
2020-11-12 16:32:34 +0100

    developing with sceneform

---
+++ application/app/src/main/java/si/labs/augmented_reality_menu/ARActivitySceneform.java
@@ -0,0 +1,14 @@
+package si.labs.augmented_reality_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ARActivitySceneform extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_r_sceneform);
+    }
+}
