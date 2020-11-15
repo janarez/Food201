05df86a00829878e504fa064a1238299c9b9cca7
Jana Rezabkova <jana.rezabkova@gmail.com>
2020-11-15 14:08:51 +0100

    Preprocess raw scene image into model input

--- application/app/src/main/java/si/labs/augmented_reality_menu/model/ModelExecutor.java
+++ application/app/src/main/java/si/labs/augmented_reality_menu/model/ModelExecutor.java
@@ -2,17 +2,21 @@ package si.labs.augmented_reality_menu.model;
 
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.util.Log;
 
+import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-
-import si.labs.augmented_reality_menu.ARActivity;
 
 public class ModelExecutor {
     private Context context;
@@ -20,22 +24,75 @@ public class ModelExecutor {
     // Runs the tf-lite model.
     private Interpreter interpreter;
 
+    // Model constants.
+    private static final String MODEL_NAME = "apple_barbecue.tflite";
+    private static final int IMAGE_HEIGHT = 64;
+    private static final int IMAGE_WIDTH = 64;
+    private static final int CLASSES = 208;
+
     // Testing variables for model timing.
     private long totalTime = 0;
-    private long preprocessingTime = 0;
+    private long preprocessTime = 0;
     private long segmentationTime = 0;
-    private long maskFlatteningTime = 0;
+    private long maskTime = 0;
 
     private static final String TAG = ModelExecutor.class.getSimpleName();
 
-    ModelExecutor(Context context, String modelName) {
+    public ModelExecutor(Context context) {
         this.context = context;
-        interpreter = getInterpreter(context, modelName);
+        interpreter = getInterpreter(context, MODEL_NAME);
     }
 
+    public Bitmap run(Image image) {
+        totalTime = System.currentTimeMillis();
+        // Image preprocessing.
+        preprocessTime = System.currentTimeMillis();
+        ByteBuffer byteBuffer = preprocessImage(image);
+        preprocessTime -= System.currentTimeMillis();
+        Log.d(TAG, String.format("Image preprocessing took %d ms", preprocessTime));
 
+        // Segmentation.
+        segmentationTime = System.currentTimeMillis();
 
-    // Initializes model interpreter with the tf-lite model.
+        segmentationTime -= System.currentTimeMillis();
+        Log.d(TAG, String.format("Segmentation took %d ms", segmentationTime));
+
+        // Mask processing.
+        maskTime = System.currentTimeMillis();
+        maskTime -= System.currentTimeMillis();
+        Log.d(TAG, String.format("Mask postprocessing took %d ms", maskTime));
+
+        totalTime -= System.currentTimeMillis();
+        Log.d(TAG, String.format("Full model run took %d ms", totalTime));
+
+        return null;
+    }
+
+    /**
+     * Converts AR scene image in YUV format to RGB bitmap and using
+     * tf support library resizes and converts to tensor.
+     */
+    private ByteBuffer preprocessImage(Image image) {
+        Bitmap bitmap = ImageUtils.fromYuvImageToRgbBitmap(image);
+
+        // Create ImageProcessor with all preprocessing steps chained.
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        // Just resizing, [0,1] transform is model layer.
+                        .add(new ResizeOp(IMAGE_HEIGHT, IMAGE_WIDTH, ResizeOp.ResizeMethod.BILINEAR))
+                        // TODO: Add rotation (from landscape)? Or not necessary?
+                        .build();
+
+        // Push bitmap to TensorImage to apply processing.
+        TensorImage tImage = new TensorImage(DataType.UINT8);
+        tImage.load(bitmap);
+        tImage = imageProcessor.process(tImage);
+        return tImage.getBuffer();
+    }
+
+    /**
+     * Initializes model interpreter with the tf-lite model.
+     */
     private Interpreter getInterpreter(Context context, String modelName) {
         Interpreter.Options interpreterOptions = new Interpreter.Options();
         // TODO: Setup thread utilization and possible GPU.
@@ -43,6 +100,9 @@ public class ModelExecutor {
         return new Interpreter(modelFile, interpreterOptions);
     }
 
+    /**
+     * Loads model from assets.
+     */
     private MappedByteBuffer getModelFile(Context context, String modelName) {
         try {
             AssetFileDescriptor fD = context.getAssets().openFd(modelName);
@@ -58,5 +118,3 @@ public class ModelExecutor {
         return null;
     }
 }
-
-
