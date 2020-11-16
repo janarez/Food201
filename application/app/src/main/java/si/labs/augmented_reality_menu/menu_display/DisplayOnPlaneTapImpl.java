e4952f05fafdaf9dcc0175a0716b825b69fb741e
Jana Rezabkova <jana.rezabkova@gmail.com>
2020-11-16 15:03:15 +0100

    Change text inside AR `TextView` dynamically

--- application/app/src/main/java/si/labs/augmented_reality_menu/menu_display/DisplayOnPlaneTapImpl.java
+++ application/app/src/main/java/si/labs/augmented_reality_menu/menu_display/DisplayOnPlaneTapImpl.java
@@ -4,14 +4,22 @@ import android.graphics.ImageFormat;
 import android.media.Image;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.widget.TextView;
 
+import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.core.exceptions.NotYetAvailableException;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.BaseArFragment;
 
+import java.util.Optional;
+
 import si.labs.augmented_reality_menu.ARActivity;
+import si.labs.augmented_reality_menu.R;
 import si.labs.augmented_reality_menu.model.ModelExecutor;
 import si.labs.augmented_reality_menu.model.ModelOutput;
 
@@ -35,13 +43,13 @@ public class DisplayOnPlaneTapImpl implements BaseArFragment.OnTapArPlaneListene
         if (frame != null) {
             // Copy the camera stream to a bitmap
             try (Image sceneImage = frame.acquireCameraImage()) {
-                Log.d(TAG, String.format("Acquired scene image [%d, %d] in format %s",
+                Log.d(TAG, String.format("Acquired scene image [%d, %d] in format %d",
                         sceneImage.getHeight(), sceneImage.getWidth(), sceneImage.getFormat()));
 
                 // The scene image should and must be in given format as per documentation.
                 if (sceneImage.getFormat() != ImageFormat.YUV_420_888) {
                     throw new UnsupportedOperationException(
-                            String.format("Cannot process scene image in format %s", sceneImage.getFormat()));
+                            String.format("Cannot process scene image in format %d (only 35 is accepted)", sceneImage.getFormat()));
                 }
 
                 // Pass to model.
@@ -53,20 +61,22 @@ public class DisplayOnPlaneTapImpl implements BaseArFragment.OnTapArPlaneListene
             }
         }
 
+        // TODO: Handle labels + mask display differently. For now put labels inside random text box.
+        Optional<ViewRenderable> menu = arActivity.getMenuRenderable();
+        if (!menu.isPresent()) {
+            return;
+        }
 
+        Anchor anchor = hitResult.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
 
+        Node menuNode = new Node();
+        menuNode.setParent(anchorNode);
+        menuNode.setRenderable(menu.get());
 
-//        Optional<ViewRenderable> menu = arActivity.getMenuRenderable();
-//        if (!menu.isPresent()) {
-//            return;
-//        }
-//
-//        Anchor anchor = hitResult.createAnchor();
-//        AnchorNode anchorNode = new AnchorNode(anchor);
-//        anchorNode.setParent(arFragment.getArSceneView().getScene());
-//
-//        Node menuNode = new Node();
-//        menuNode.setParent(anchorNode);
-//        menuNode.setRenderable(menu.get());
+        // Display the obtained labels.
+        TextView textBox = menu.get().getView().findViewById(R.id.labelText);
+        textBox.setText("test");
     }
 }
